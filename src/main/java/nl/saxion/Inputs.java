ad6ba9cf11addb28a1e4cc147e24bccca29e31d3
Rizelthomas <497836@student.saxion.nl>
2022-12-21 10:14:34 +0100

    New inputs class, New static Inputs to provide input functions

--- src/main/java/nl/saxion/Inputs.java
+++ src/main/java/nl/saxion/Inputs.java
@@ -1,161 +1,21 @@
 package nl.saxion;
 
-import nl.saxion.Models.*;
-import nl.saxion.facade.PrinterFacade;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+
 import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
 import java.util.*;
-public class Main {
 
-    PrinterFacade facade = new PrinterFacade();
-    PrinterManager manager = new PrinterManager();
-    PrintTaskManager taskManager = new PrintTaskManager();
-    Menu menu = new Menu();
-    Scanner scanner = new Scanner(System.in);
+public class Inputs {
+    static Scanner scanner = new Scanner(System.in);
 
-    private String printStrategy = "Less Spool Changes";
-
-    // Run run() method
-    public static void main(String[] args) {
-        new Main().run(args);
-    }
-
-    // Read data and loop menu options
-    public void run(String[] args) {
-        if(args.length > 0) {
-            readPrintsFromFile(args[0]);
-            readSpoolsFromFile(args[1]);
-            readPrintersFromFile(args[2]);
-        } else {
-            readPrintsFromFile("");
-            readSpoolsFromFile("");
-            readPrintersFromFile("");
-        }
-
-        menu.menuSwitch();
-    }
-
-    // Start printer queue
-    public void startPrintQueue() {
-        System.out.println("<<---------- Starting Print Queue ---------->");
-        taskManager.startInitialQueue();
-        System.out.println("<----------------------------------->>");
-    }
-
-    // Exit (does nothing)
-    private void exit() {
-
-    }
-    // This method only changes the name but does not actually work.
-    // It exists to demonstrate the output.
-    // in the future strategy might be added.
-    public void changePrintStrategy() {
-        System.out.println("<<---------- Change Strategy ------------->");
-        System.out.println("- Current strategy: " + printStrategy);
-        System.out.println("- 1: Less Spool Changes");
-        System.out.println("- 2: Efficient Spool Usage");
-        System.out.println("- Choose strategy: ");
-        int strategyChoice = numberInput(1, 2);
-        if(strategyChoice == 1) {
-            printStrategy = "- Less Spool Changes";
-        } else if( strategyChoice == 2) {
-            printStrategy = "- Efficient Spool Usage";
-        }
-        System.out.println("<----------------------------------->>");
-    }
-    // TODO: This should be based on which printer is finished printing.
-    public void registerPrintCompletion() {
-        ArrayList<Printer> printers = manager.getPrinters();
-        System.out.println("<<---------- Currently Running Printers ---------->");
-        for(Printer p: printers) {
-            PrintTask printerCurrentTask= taskManager.getPrinterCurrentTask(p);
-            if(printerCurrentTask != null) {
-                System.out.println("- " + p.getId() + ": " +p.getName() + " - " + printerCurrentTask);
-            }
-        }
-        System.out.print("- Printer that is done (ID): ");
-        int printerId = numberInput(-1, printers.size());
-        System.out.println("<----------------------------------->>");
-        manager.registerCompletion(printerId);
-    }
-    public void registerPrinterFailure() {
-        ArrayList<Printer> printers = manager.getPrinters();
-        System.out.println("<<---------- Currently Running Printers ---------->");
-        for(Printer p: printers) {
-            PrintTask printerCurrentTask= taskManager.getPrinterCurrentTask(p);
-            if(printerCurrentTask != null) {
-                System.out.println("- " + p.getId() + ": " +p.getName() + " > " + printerCurrentTask);
-            }
-        }
-        System.out.print("- Printer ID that failed: ");
-        int printerId = numberInput(1, printers.size());
-        manager.registerPrinterFailure(printerId);
-        System.out.println("<----------------------------------->>");
-    }
-    public void addNewPrintTask() {
-        List<String> colors = new ArrayList<>();
-        var prints = taskManager.getPrints();
-        System.out.println("<<---------- New Print Task ---------->");
-        System.out.println("<---------- Available prints ----------");
-        int counter = 1;
-        for (var p : prints) {
-            System.out.println("- " + counter + ": " + p.getName());
-            counter++;
-        }
-        System.out.print("- Print number: ");
-        int printNumber = numberInput(1, prints.size());
-        System.out.println("-------------------------------------->");
-        Print print = taskManager.findPrint(printNumber - 1);
-        String printName = print.getName();
-        System.out.println("<---------- Filament Type ----------");
-        System.out.println("- 1: PLA");
-        System.out.println("- 2: PETG");
-        System.out.println("- 3: ABS");
-        System.out.print("- Filament type number: ");
-        int ftype = numberInput(1, 3);
-        System.out.println("-------------------------------------->");
-        FilamentType type;
-        switch (ftype) {
-            case 1 -> type = FilamentType.PLA;
-            case 2 -> type = FilamentType.PETG;
-            case 3 -> type = FilamentType.ABS;
-            default -> {
-                System.out.println("- Not a valid filamentType, bailing out");
-                return;
-            }
-        }
-        var spools = manager.getSpools();
-        System.out.println("<---------- Colors ----------");
-        ArrayList<String> availableColors = new ArrayList<>();
-        counter = 1;
-        for (var spool : spools) {
-            String colorString = spool.getColor();
-            if(type == spool.getFilamentType() && !availableColors.contains(colorString)) {
-                System.out.println("- " + counter + ": " + colorString + " (" + spool.getFilamentType() + ")");
-                availableColors.add(colorString);
-                counter++;
-            }
-        }
-        System.out.print("- Color number: ");
-        int colorChoice = numberInput(1, availableColors.size());
-        colors.add(availableColors.get(colorChoice-1));
-        for(int i = 1; i < print.getFilamentLength().size(); i++) {
-            System.out.print("- Color number: ");
-            colorChoice = numberInput(1, availableColors.size());
-            colors.add(availableColors.get(colorChoice-1));
-        }
-        System.out.println("-------------------------------------->");
-        taskManager.addPrintTask(printName, colors, type);
-        System.out.println("<---------------------------->>");
-    }
-    public void showPrints() {
+    public static void showPrints() {
         var prints = taskManager.getPrints();
         System.out.println("<<---------- Available prints ---------->");
         for (var p : prints) {
@@ -163,6 +23,7 @@ public class Main {
         }
         System.out.println("<-------------------------------------->>");
     }
+
     public void showSpools() {
         var spools = manager.getSpools();
         System.out.println("<<---------- Spools ---------->");
@@ -171,6 +32,7 @@ public class Main {
         }
         System.out.println("<---------------------------->>");
     }
+
     public void showPrinters() {
         var printers = manager.getPrinters();
         System.out.println("<<--------- Available printers --------->");
@@ -185,6 +47,7 @@ public class Main {
         }
         System.out.println("<-------------------------------------->>");
     }
+
     public void showPendingPrintTasks() {
         ArrayList<PrintTask> printTasks = taskManager.getPendingPrintTasks();
         System.out.println("<<--------- Pending Print Tasks --------->");
@@ -193,13 +56,12 @@ public class Main {
         }
         System.out.println("<-------------------------------------->>");
     }
+
     private void readPrintsFromFile(String filename) {
         JSONParser jsonParser = new JSONParser();
         if(filename.length() == 0) {
             filename = "prints.json";
         }
-
-        System.out.println(Main.class.getResource("Main.class"));
 
         URL printResource = getClass().getResource("/" + filename);
         if (printResource == null) {
@@ -229,6 +91,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+
     private void readPrintersFromFile(String filename) {
         JSONParser jsonParser = new JSONParser();
         if(filename.length() == 0) {
@@ -257,6 +120,7 @@ public class Main {
             e.printStackTrace();
         }
     }
+
     private void readSpoolsFromFile(String filename) {
         JSONParser jsonParser = new JSONParser();
         if(filename.length() == 0) {
@@ -291,6 +155,12 @@ public class Main {
             e.printStackTrace();
         }
     }
+
+    /**
+     * Wait for and grab the latest string input from stdin.
+     *
+     * @return The line read by scanner.
+     */
     public String stringInput() {
         String input = null;
         while(input == null || input.length() == 0){
@@ -298,13 +168,28 @@ public class Main {
         }
         return input;
     }
-    public int numberInput() {
+
+    /**
+     * Wait for and grab the latest number input from stdin.
+     *
+     * @return The number entered.
+     */
+    public static int numberInput() {
         return scanner.nextInt();
     }
-    public int numberInput(int min, int max) {
-        int input = numberInput();
+
+    /**
+     * Loop to request a new number from stdin if the number isn't within the
+     * specified bounds.
+     *
+     * @param min The lower bound the input must satisfy.
+     * @param max The upper bound the input must satisfy.
+     * @return A valid number input.
+     */
+    public static int numberInput(int min, int max) {
+        int input = Inputs.numberInput();
         while (input < min || input > max) {
-            input = numberInput();
+            input = Inputs.numberInput();
         }
         return input;
     }
