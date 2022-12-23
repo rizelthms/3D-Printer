7849b9360c65a44517ac3afdf9330f5e2d91b1a5
Rizelthomas <497836@student.saxion.nl>
2022-12-23 15:03:53 +0100

    Switch enum back to var-less setup

--- src/main/java/nl/saxion/Models/MainMenuOptions.java
+++ src/main/java/nl/saxion/Models/MainMenuOptions.java
@@ -1,20 +1,14 @@
 package nl.saxion.Models;
 
 public enum MainMenuOptions {
-        InvalidOption(0),
-        AddNewPrintTask(1),
-        RegisterPrintCompletion(2),
-        RegisterPrinterFailure(3),
-        ChangePrintStrategy(4),
-        StartPrintQueue(5),
-        ShowPrints(6),
-        ShowPrinters(7),
-        ShowSpools(8),
-        ShowPendingPrintTasks(9);
-
-        private int option;
-
-        MainMenuOptions (int option) {
-            this.option = option;
-        }
-    }
+        InvalidOption,
+        AddNewPrintTask,
+        RegisterPrintCompletion,
+        RegisterPrinterFailure,
+        ChangePrintStrategy,
+        StartPrintQueue,
+        ShowPrints,
+        ShowPrinters,
+        ShowSpools,
+        ShowPendingPrintTasks;
+}
