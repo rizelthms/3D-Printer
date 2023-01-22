6c10b8dc4265a5ad3d959fca2870f632ed0e5665
Rizelthomas <497836@student.saxion.nl>
2023-01-22 17:28:43 +0100

    Get all valid spools for task from all free spools

--- src/main/java/nl/saxion/Models/PrintTask.java
+++ src/main/java/nl/saxion/Models/PrintTask.java
@@ -1,19 +1,18 @@
 package nl.saxion.Models;
 
+import nl.saxion.utils.Tools;
 import java.util.List;
-import java.util.UUID;
+import java.util.ArrayList;
 
 public class PrintTask {
     private Print print;
     private List<String> colors;
     private FilamentType filamentType;
 
-
     public PrintTask(Print print, List<String> colors, FilamentType filamentType){
         this.print = print;
         this.colors = colors;
         this.filamentType = filamentType;
-
     }
 
     public List<String> getColors() {
@@ -30,6 +29,24 @@ public class PrintTask {
 
     @Override
     public String toString() {
-        return "< " + print.getName() +" " + filamentType + " " + colors.toString() + " >";
+        return "< " + print.getName() + " " + filamentType + " " + colors.toString() + " >";
+    }
+
+    public ArrayList<Spool> getValidSpools(ArrayList<Spool> spools) {
+        ArrayList<Spool> validSpools = new ArrayList<Spool>();
+
+        for (int i = 0; i < colors.size(); i++) {
+            for (Spool spool : spools) {
+                String curColor = colors.get(i);
+                boolean spoolMatched = spool.spoolMatch(curColor, filamentType);
+
+                if (spoolMatched && !Tools.containsSpool(spools, curColor)) {
+                    validSpools.add(spool);
+                    break; // Match only one spool per color.
+                }
+            }
+        }
+
+        return validSpools;
     }
 }
